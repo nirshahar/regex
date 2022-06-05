1031a10edf687fad6fd2260f8b95a20a69bb9f7d
nir <nirshahar@mail.tau.ac.il>
2022-06-05 18:40:37 +0300

    Add simple state transitions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+use state_machine::{StateMachine, StateType};
+
+use crate::state_machine::SimpleTransition;
+
 pub mod state_machine;
 
 fn main() {
-    println!("Hello, world!");
+    let mut state_machine = StateMachine::<char, ()>::new(StateType::Regular);
+
+    let starting = state_machine.starting_state;
+    let s1 = state_machine.add_state(StateType::Regular);
+    let s2 = state_machine.add_state(StateType::Accepting);
+
+    state_machine.set_transition(
+        starting,
+        Box::new(SimpleTransition::from_iter([('a', starting), ('b', s1)])),
+    );
+
+    state_machine.set_transition(
+        s1,
+        Box::new(SimpleTransition::from_iter([('b', s1), ('c', s2)])),
+    );
+
+    state_machine.set_transition(s2, Box::new(SimpleTransition::from_iter([('c', s2)])));
+
+    println!("{}", state_machine.run(vec!['c', 'b', 'a', 'a']));
+    println!("{}", state_machine.run(vec!['a', 'b', 'b', 'c', 'c']));
+    println!("{}", state_machine.run(vec!['a', 'b', 'c']));
+    println!("{}", state_machine.run(vec!['b', 'c']));
+    println!("{}", state_machine.run(vec!['a', 'c']));
 }
--- src/state_machine.rs
+++ src/state_machine.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash};
+
 #[derive(Debug, Clone, Copy)]
 pub enum StateType {
     Regular,
@@ -8,7 +10,7 @@ pub enum StateType {
 pub struct State(usize);
 
 pub trait StateTransformer<Item: Sized, StackItem: Sized> {
-    fn transition(&self, input: &mut Vec<Item>, stack: &mut Vec<StackItem>) -> State;
+    fn transition(&self, input: &mut Vec<Item>, stack: &mut Vec<StackItem>) -> Option<State>;
 }
 
 pub struct StateMachine<Item, StackItem> {
@@ -39,7 +41,7 @@ impl<Item, StackItem> StateMachine<Item, StackItem> {
         self.transitions.push(None);
         self.state_types.push(state_type);
 
-        State(self.len())
+        State(self.len() - 1)
     }
 
     pub fn set_transition(
@@ -54,7 +56,9 @@ impl<Item, StackItem> StateMachine<Item, StackItem> {
         self.transitions[state.0] = None;
     }
 
-    pub fn run(&self, input: &mut Vec<Item>) -> bool {
+    pub fn run(&self, mut input: Vec<Item>) -> bool {
+        input.reverse(); // Reverse the input so that it will
+
         let mut stack = Vec::new();
 
         let mut cur_state = self.starting_state;
@@ -65,12 +69,53 @@ impl<Item, StackItem> StateMachine<Item, StackItem> {
                 None => return false,
             };
 
-            cur_state = transition.transition(input, &mut stack);
+            cur_state = match transition.transition(&mut input, &mut stack) {
+                Some(next_state) => next_state,
+                None => return false,
+            }
         }
 
         match self.state_types[cur_state.0] {
             StateType::Accepting => true,
             StateType::Regular => false,
         }
+    }
+}
+
+pub struct SimpleTransition<Item> {
+    transitions: HashMap<Item, State>,
+}
+
+impl<Item> SimpleTransition<Item>
+where
+    Item: Eq + Hash,
+{
+    pub fn new() -> Self {
+        SimpleTransition {
+            transitions: HashMap::new(),
+        }
+    }
+
+    pub fn from_iter<T>(iter: T) -> Self
+    where
+        T: IntoIterator<Item = (Item, State)>,
+    {
+        SimpleTransition {
+            transitions: HashMap::from_iter(iter),
+        }
+    }
+
+    pub fn add_transition(&mut self, inp_letter: Item, next_state: State) {
+        self.transitions.insert(inp_letter, next_state);
+    }
+}
+
+impl<Item> StateTransformer<Item, ()> for SimpleTransition<Item>
+where
+    Item: Eq + Hash,
+{
+    fn transition(&self, input: &mut Vec<Item>, _: &mut Vec<()>) -> Option<State> {
+        let inp = input.pop()?;
+        self.transitions.get(&inp).map(|&state| state)
     }
 }
